7158c18d0ac0fcc32fa99527ba1b1086d2d88fae
Alexander Daurskikh <sanya219@gmail.com>
2023-02-20 09:04:39 +0800

    task 50 solved

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ m = 3, n = 4.
 8 7,8 -7,1 9
 */
 
-double[,] GenerateArray2D (int rows, int cols, int min, int max)
+double[,] GenerateArray2DDbl (int rows, int cols, int min, int max)
 {
     double[,] array = new double[rows, cols];
     for(int i = 0; i < rows; i++)
@@ -17,7 +17,7 @@ double[,] GenerateArray2D (int rows, int cols, int min, int max)
     return array;
 }
 
-void PrintArray2D (double[,] array)
+void PrintArray2DDbl (double[,] array)
 {
     for(int i = 0; i < array.GetLength(0); i++)
     {
@@ -36,8 +36,8 @@ int min = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter maximal integer value: ");
 int max = Convert.ToInt32(Console.ReadLine());
 
-double[,] myArray = GenerateArray2D(rows, cols, min, max);
-PrintArray2D(myArray);
+double[,] myArray47 = GenerateArray2DDbl(rows, cols, min, max);
+PrintArray2DDbl(myArray47);
 
 /* Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение 
 этого элемента или же указание, что такого элемента нет.
@@ -47,3 +47,33 @@ PrintArray2D(myArray);
 8 4 2 4
 1 7 -> числа с такими индексами в массиве нет
 */
+
+int[,] GenerateArray2DInt (int rows, int cols, int min, int max)
+{
+    int[,] array = new int[rows, cols];
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < cols; j++)
+            array[i, j] = new Random().Next(min, max + 1);
+    return array;
+}
+
+void PrintArray2DInt (int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write((array[i, j] < 0 ? "": " ") + array[i, j] + "\t");
+        Console.Write("\n");
+    }
+}
+
+Console.Write("Enter number of rows: ");
+rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number of columns: ");
+cols = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter minimal value: ");
+min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter maximal value: ");
+max = Convert.ToInt32(Console.ReadLine());
+int[,] myArray50 = GenerateArray2DInt(rows, cols, min, max);
+PrintArray2DInt(myArray50);
